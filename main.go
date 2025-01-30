29fa294da142029e88c18bd7c75c51f681d8581d
yanitski <yanitski@ph1.uni-koeln.de>
2025-01-30 12:14:02 -0700

    updated fileserver address and added readiness endpoint

--- main.go
+++ main.go
@@ -10,15 +10,25 @@ func main() {
     mux := http.NewServeMux()
 
     // Define multiplexer handle
-    mux.Handle("/", http.FileServer(http.Dir(".")))
+    mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(200)
+        w.Write([]byte("OK"))
+    })
 
     // Define server
-    server := http.Server{Addr: ":8080", Handler: mux}
+    port := "8080"
+    server := http.Server{
+        Addr:    ":" + port, 
+        Handler: mux,
+    }
 
     // Start server
+    fmt.Printf("Serving files from / on port: %v\n", port)
     err := server.ListenAndServe()
     if err != nil {
         panic(err)
     }
-    fmt.Println("Server started...")
 }
+
