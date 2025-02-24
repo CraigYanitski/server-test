f0f5b54798bcf1a17a5c6723efdc97cda8ca1a58
yanitski <yanitski@ph1.uni-koeln.de>
2025-02-23 21:45:05 -0700

    new schema and queries for the database refresh_tokens

--- sql/schema/004_refresh-tokens.sql
+++ sql/schema/004_refresh-tokens.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE refresh_tokens (
-    token UUID PRIMARY KEY,
+    token TEXT PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     user_id UUID NOT NULL REFERENCES users ON DELETE CASCADE,
     expires_at TIMESTAMP NOT NULL,
-    revoked_at TIMESTAMP NULL
+    revoked_at TIMESTAMP
 ) ;
 
 -- +goose Down
